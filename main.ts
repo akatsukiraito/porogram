9c1f6f55913c4415c77556d4d25712373993c121
akatsukiraito <67031795+akatsukiraito@users.noreply.github.com>
2020-06-24 12:04:52 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,36 @@
 radio.onReceivedNumber(function (receivedNumber) {
     let 相手の手２ = 0
-    let 相手の手１ = 0
-    if (相手の手１ && 相手の手２) {
-    	
+    if (相手の手１ == 2 && 相手の手２ == 2) {
+        自分の手 = 1
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.showIcon(IconNames.No)
+        if (相手の手１ == 1 && 相手の手２ == 1) {
+            自分の手 = 3
+            basic.showIcon(IconNames.Heart)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+        if (相手の手１ == 3 && 相手の手２ == 3) {
+            自分の手 = 2
+            basic.showIcon(IconNames.Heart)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+        if (相手の手１ == 相手の手２ && 相手の手２ == 自分の手) {
+            basic.showIcon(IconNames.Triangle)
+        }
     }
 })
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Scissors)
-    radio.sendNumber(1)
+    radio.sendNumber(2)
+    自分の手 = 2
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Square)
     radio.sendNumber(3)
+    相手の手１ = 3
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
@@ -21,8 +40,11 @@ input.onButtonPressed(Button.B, function () {
         . # # # .
         . . . . .
         `)
-    radio.sendNumber(2)
+    radio.sendNumber(1)
+    自分の手 = 1
 })
+let 自分の手 = 0
+let 相手の手１ = 0
 basic.showIcon(IconNames.Yes)
 basic.forever(function () {
     radio.setGroup(8)
